7414fa184b66b3860e6793523f706d8bb0d5ec24
Marcus Brummer <mbrlabs7@gmail.com>
2017-01-14 16:55:39 +0100

    Basic setup

---
+++ src/pixl/image.h
@@ -0,0 +1,16 @@
+#ifndef PIXL_IMAGE_H
+#define PIXL_IMAGE_H
+
+#include "types.h"
+
+namespace pixl {
+
+    class Image {
+    public:
+        Image();
+        ~Image();
+    };
+
+}
+
+#endif
---
+++ src/pixl/types.h
@@ -0,0 +1,18 @@
+#ifndef PIXL_TYPES_H
+#define PIXL_TYPES_H
+
+namespace pixl {
+    typedef char i8;
+    typedef short i16;
+    typedef int i32;
+    typedef long i64;
+    typedef float f32;
+    typedef double f64;
+
+    typedef unsigned char u8;
+    typedef unsigned short u16;
+    typedef unsigned int u32;
+    typedef unsigned long u64;
+}
+
+#endif
