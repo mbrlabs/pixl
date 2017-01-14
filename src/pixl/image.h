4e2e3e081ae8a744a72848cd7e720d9fadfe8a22
Marcus Brummer <mbrlabs7@gmail.com>
2017-01-14 17:09:39 +0100

    image

--- src/pixl/image.h
+++ src/pixl/image.h
@@ -9,6 +9,15 @@ namespace pixl {
     public:
         Image();
         ~Image();
+        inline u8* getData() { return this->data; }
+        inline u32 getWidth() { return this->width; }
+        inline u32 getHeight() { return this->height; }
+        inline u32 getSize() { return this->size; }
+    private:
+        u32 width;
+        u32 height;
+        u32 size;
+        u8* data;
     };
 }
 
