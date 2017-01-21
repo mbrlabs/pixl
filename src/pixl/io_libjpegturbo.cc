fecfac3aac1dcba5b9583632430c7bb6584f0ecc
Marcus Brummer <mbrlabs7@gmail.com>
2017-01-21 21:19:30 +0100

    Implemented libjpegturbo reader

--- src/pixl/io_libjpegturbo.cc
+++ src/pixl/io_libjpegturbo.cc
@@ -17,16 +17,75 @@
 #include <turbojpeg.h>
 
 #include "io.h"
+#include "types.h"
 #include "utils.h"
+
 
 namespace pixl {
 
-    Image* JpegTurboReader::read(const char* path) {
-        PIXL_DEBUG("turbo: " << TJ_NUMXOP);
-        auto x = tjInitCompress();
+	// ----------------------------------------------------------------------------
+	u8* read_binary(const char* path, u64* length) {
+	    FILE* file = fopen(path, "rb");
+	    
+	    // read file size
+	    fseek(file, 0, SEEK_END);
+	    *length = ftell(file);
+	    
+	    // setup data
+	    u8* data = new u8[*length + 1];
 
-        StbiReader reader;
-        return reader.read(path);
+	    // read file
+	    fseek(file, 0, SEEK_SET);
+	    fread(data, 1, *length, file);
+	    fclose(file);
+
+	    return data;
+	}
+
+    Image* JpegTurboReader::read(const char* path) {
+        // read file
+        u64 dataSize;
+        u8* dataBuffer = read_binary(path, &dataSize);
+
+        // jpg meta data
+        int width, height, subsamp;
+
+        // read meta data
+        auto decoder = tjInitDecompress();
+        auto result = tjDecompressHeader2(decoder, dataBuffer, dataSize, &width, &height, &subsamp);
+
+        if(result == -1) {
+ 			PIXL_ERROR("Error: " + std::string(tjGetErrorStr()));
+        	return nullptr;
+        }
+
+		PIXL_DEBUG("header decoding success");
+		PIXL_DEBUG("width: " << width);
+		PIXL_DEBUG("height: " << height);
+		PIXL_DEBUG("subsamp: " << subsamp);
+
+		// create decoded buffer
+		int pitch = width * tjPixelSize[TJPF_RGB];
+		u8* data = (u8*)malloc(pitch * height); 
+
+		result = tjDecompress2(
+			decoder,
+			dataBuffer,
+			dataSize,
+			data,
+			width, 
+			pitch,
+			height, 
+			TJPF_RGB,
+			0 // flags
+		);
+
+		if(result == -1) {
+ 			PIXL_ERROR("Error: " + std::string(tjGetErrorStr()));
+ 			return nullptr;
+		}
+
+		return new Image(width, height, 3, data);
     }
 
     void JpegTurboWriter::write(const char* path, Image* image) {
