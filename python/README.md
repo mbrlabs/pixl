5cbceb62ed13acded7d05215094e82ed0a5d1e15
Marcus Brummer <mbrlabs7@gmail.com>
2017-01-22 21:56:02 +0100

    Python setup

---
+++ python/README.md
@@ -0,0 +1,5 @@
+# Python bindings for pixl
+
+This python package is a wrapper around the C API of pixl.    
+Pixl is a high performance image processing library written in C++.
+
---
+++ python/setup.py
@@ -0,0 +1,30 @@
+#
+# Copyright (c) 2017. See AUTHORS file.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from distutils.core import setup
+
+setup(
+    name = 'pixl',
+    packages = ['pixl'],
+    version = '0.1.0',
+    description = 'Python bindings for the pixl image processing library',
+    author = 'Marcus Brummer',
+    author_email = 'mbrlabs7@gmail.com',
+    url = 'https://github.com/mbrlabs/pixl/python',
+    download_url = '', # TODO
+    keywords = ['image', 'processing' ], # TODO
+    classifiers = [], # TODO
+)
