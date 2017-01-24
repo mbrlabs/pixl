781d46720654cefaabc74a3a47ec2a7ff3c75ba2
Marcus Brummer <mbrlabs7@gmail.com>
2017-01-24 01:47:01 +0100

    Python bindings

--- python/pixl/__init__.py
+++ python/pixl/__init__.py
@@ -14,37 +14,63 @@
 # limitations under the License.
 #
 
+"""
+
+TODO package docstring
+
+"""
+
 import enum
-import ctypes
+from ctypes import *
 
-libpixl = ctypes.cdll.LoadLibrary('libpixl.so')
-
-class IMAGE(ctypes.Structure):
-	_fields_ = [('width', ctypes.c_uint),
-				('height', ctypes.c_uint),
-				('__handle', ctypes.c_void_p)]
-
-libpixl.pixl_load_image.argtypes = [ctypes.c_char_p]
-libpixl.pixl_load_image.restype = ctypes.POINTER(IMAGE)
-
+# default values
 PIXL_THREADS = 1
+
+# load the shared library
+_LIBPIXL = cdll.LoadLibrary('libpixl.so')
+
+class IMAGE(Structure):
+	_fields_ = [('width', c_uint),
+				('height', c_uint),
+				('__handle', c_void_p)]
+
+# extern c: pixl_load_image
+_LIBPIXL.pixl_load_image.argtypes = [c_char_p]
+_LIBPIXL.pixl_load_image.restype = POINTER(IMAGE)
+
+# extern c: pixl_save_image
+_LIBPIXL.pixl_save_image.argtypes = [c_char_p, POINTER(IMAGE)]
+
+#extern c: pixl_flip
+_LIBPIXL.pixl_flip.argtypes = [POINTER(IMAGE), c_int, c_uint]
+
+#extern c: pixl_resize
+_LIBPIXL.pixl_resize.argtypes = [POINTER(IMAGE), c_uint, c_uint, c_uint]
 
 class Orientation(enum.Enum):
 	VERTICAL = 0
 	HORIZONTAL = 1
 
+
 class Image():
 	def __init__(self, path):
-		self.__IMAGE = libpixl.pixl_load_image(ctypes.c_char_p(path.encode()))
+		self._IMAGE = _LIBPIXL.pixl_load_image(c_char_p(path.encode()))
 
 	def destroy(self):
-		libpixl.pixl_destroy_image(self.__IMAGE)
+		libpixl.pixl_destroy_image(self._IMAGE)
 
 	def width(self):
-		return self.__IMAGE.contents.width
+		return self._IMAGE.contents.width
 
 	def height(self):
-		return self.__IMAGE.contents.height
+		return self._IMAGE.contents.height
 
-def test():
-	libpixl.pixl_say_hello()
+
+def flip(image, orientation = Orientation.HORIZONTAL):
+	_LIBPIXL.pixl_flip(image._IMAGE, orientation.value, PIXL_THREADS)
+
+def resize(image, width, height):
+	_LIBPIXL.pixl_resize(image._IMAGE, int(width), int(height), PIXL_THREADS)
+
+def save_image(path, image):
+	_LIBPIXL.pixl_save_image(c_char_p(path.encode()), image._IMAGE)
